2c54f85a59f55267df326918aea22bb1ef9b41a0
SantamRC <santamdev404@gmail.com>
2022-06-03 01:42:43 +0530

    Added Client files

--- server.ts
+++ server.ts
@@ -1,7 +1,17 @@
 require("dotenv");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(`mongodb://localhost:27017`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected Successful"))
+  .catch((err:any) => console.log(`Connection failed ! Error : ${err}`));
+
 
 app.listen(port, () => {
   console.log("listening on port " + port);
